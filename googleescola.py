ff1bb630593fb9373adcd920c25b2cce9f4e7501
Vinicius Mangueira <viniciusmangueira04@gmail.com>
2025-03-07 09:09:58 -0300

    Adicionando artigos em ingles

--- googleescola.py
+++ googleescola.py
@@ -1,12 +1,23 @@
 from scholarly import scholarly as sch
 
-busca = "Centro de Informática UFPB"
+# Adicionando termos em inglês na busca
+busca = "Centro de Informática UFPB OR 'Center for Informatics UFPB'"
 resultados_busca = sch.search_pubs(busca)
 
 for i in range(100): 
-    artigo = next(resultados_busca)  
-    print(f"Título: {artigo['bib']['title']}")
-    print(f"Autores: {artigo['bib'].get('author', 'N/A')}")
-    print(f"Ano: {artigo['bib'].get('pub_year', 'N/A')}")
-    print(f"Link: {artigo.get('pub_url', 'Sem link disponível')}")
-    print("-" * 50)
+    try:
+        artigo = next(resultados_busca)
+        titulo = artigo['bib']['title']
+        autores = artigo['bib'].get('author', 'N/A')
+        ano = artigo['bib'].get('pub_year', 'N/A')
+        link = artigo.get('pub_url', 'Sem link disponível')
+
+        print(f"Título: {titulo}")
+        print(f"Autores: {autores}")
+        print(f"Ano: {ano}")
+        print(f"Link: {link}")
+        print("-" * 50)
+    
+    except StopIteration:
+        print("Sem mais resultados disponíveis.")
+        break
